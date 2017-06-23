43e59cbbbd5f99b7b5374cacc19e1e05912893fb
Jan Provaznik <jprovazn@redhat.com>
2017-06-23 14:32:47 +0200

    puppet-ganesha: Initial commit
    This is the initial commit for puppet-ganesha.
    
    Change-Id: Idaeeea859c9a7020e444c770f5f1c0b9d56e639d

---
+++ manifests/config.pp
@@ -0,0 +1,36 @@
+# == Class ganesha::config
+#
+# This class handles ganesha config changes.
+#
+class ganesha::config {
+  file { '/etc/ganesha':
+    ensure  => directory,
+    owner   => 0,
+    group   => 0,
+    mode    => '0775',
+    recurse => true,
+  }
+
+  file { '/etc/ganesha/export.d':
+    ensure  => directory,
+    owner   => 0,
+    group   => 0,
+    mode    => '0775',
+    recurse => true,
+  }
+
+  file { '/etc/ganesha/ganesha.conf':
+    ensure  => file,
+    owner   => 0,
+    group   => 0,
+    mode    => '0644',
+    content => template('ganesha/ganesha.conf.erb'),
+  }
+
+  file { '/etc/ganesha/export.d/INDEX.conf':
+    ensure => file,
+    owner  => 0,
+    group  => 0,
+    mode   => '0644',
+  }
+}
