f8852e6ddcafce0db23d3750d0ae37e740306e7e
Jan Provaznik <jprovazn@redhat.com>
2017-06-23 14:32:47 +0200

    puppet-ganesha: Initial commit
    This is the initial commit for puppet-ganesha.
    
    Change-Id: Idaeeea859c9a7020e444c770f5f1c0b9d56e639d

---
+++ spec/acceptance/ganesha_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper_acceptance'
+
+describe 'basic ganesha' do
+
+  context 'default parameters' do
+
+    it 'should install ganesha server' do
+      pp = <<-EOS
+        if $::operatingsystem == 'CentOS' {
+          class { '::ganesha':
+            yum_base_url => 'http://download.ceph.com/nfs-ganesha/rpm-V2.5-stable/el7/$basearch/',
+          }
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      # Run it twice and test for idempotency
+      # TODO: ubuntu now relies on nightly builds, fetching
+      # its repo always overwrites repo file so there
+      # is always change, check for changes on centos for now
+      if os[:family].casecmp('RedHat') == 0
+        apply_manifest(pp, :catch_changes => true)
+      end
+    end
+
+    describe port(2049) do
+      it { is_expected.to be_listening }
+    end
+  end
+
+end
